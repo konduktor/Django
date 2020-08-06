139aa5dda542fccc23ed05c78999a1e41bf6e9d6
konduktor <hvoschov.kirill@yadenx.ru>
2020-08-06 23:28:11 +0300

    sixth commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -122,15 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
 
 LOGIN_REDIRECT_URL = "tasks:list"
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hvoschov.kirill@gmail.com'
